b073595e2ae67dd85d80406187a028523ad7b314
Sami Mäläskä <epsilon@welho.com>
2019-12-30 00:08:24 +0200

    More faults

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -22,8 +22,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
  
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
-        http.formLogin().permitAll();
+        http.headers().frameOptions().sameOrigin();
+        http.csrf().disable();
+        http.authorizeRequests()
+            //.antMatchers("/admin/**").hasRole("ADMIN")
+            .anyRequest().authenticated().and()
+            .formLogin().permitAll().and()
+            .logout().permitAll();
     }
 
     @Autowired
